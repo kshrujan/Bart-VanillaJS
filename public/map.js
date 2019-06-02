cdc542f51f60fbad75e211f3699b4e669c801f8c
Shrujan Kalidindi <shrujank@gmail.com>
2019-06-02 01:44:30 -0700

    Trying something

---
+++ public/map.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    function initMap() {
+        window.directionsService = new google.maps.DirectionsService();
+        window.directionsDisplay = new google.maps.DirectionsRenderer();
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 37.773972, lng: -122.431297},
+            zoom: 10
+        });
+        directionsDisplay.setMap(map);
+    }
+    initMap();
+});
