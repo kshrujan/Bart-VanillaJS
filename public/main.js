57902c5bb7523ab9c4bc82cf1f4926af224b7f04
Shrujan Kalidindi <shrujank@gmail.com>
2019-06-02 00:59:13 -0700

    Final push

--- public/main.js
+++ public/main.js
@@ -2,22 +2,104 @@ var startStation = $("#startStation");
 var endStation = $("#endStation");
 var sideBar = $("#sidebarId");
 var submit = $("#formSubmit");
+var tableDiv = $("#tableDiv");
+var table = $("#scheduleTable tbody");
+var timeRemaining = $("#time");
+var timeRemainingDiv = $("#timeBeforeNextTrain");
+var welcomeBanner = $("#welcomeBanner");
 
+//global vars
+var firstAvailableTrain = "";
+var departureStationData = {};
+var arrivalStationData = {};
+
+//window based functions
 window.onload = event => {
+    if(localStorage.getItem("visit")) {
+        let visits = parseInt(localStorage.getItem("visit"));
+        visits+=1;
+        localStorage.setItem("visit", visits);
+    } else {
+        localStorage.setItem("visit", 0);
+    }
+
+    //set the banner
+    if(parseInt(localStorage.getItem("visit"), 10) > 0) {
+        welcomeBanner.html("Welcome Back! Visits: " + localStorage.getItem("visit"));
+    } else {
+        welcomeBanner.html("Welcome!");
+    }
     //get the list of stations
-    $.get("http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y", function(data) {
+    $.get("http://localhost:8081/stations", function(data) {
         let stationData = data["root"]["stations"]["station"];
         $.each(stationData, (index) => {
             startStation.append($("<option></option>").attr("value",stationData[index]["abbr"]).text(stationData[index]["name"]));
-
             endStation.append($("<option></option>").attr("value",stationData[index]["abbr"]).text(stationData[index]["name"]));
         });
-    })
+    });
+
 };
 
+// setInterval(() =>{
+//     if(startStation.val() !== "Select" && endStation.val() !== "Select") {
+//         setSchedule();
+//     }
+// }, 30000);
 
+var trainTimer = (seconds) => setInterval(function() {
+    if(timeRemaining !== "") {
+        seconds--;
+        if(seconds < 0) {
+            clearInterval(trainTimer);
+        } else {
+            timeRemaining.text(seconds);
+        }
+    }
+}, 1000);
+
+// //add on change listener
+endStation.on('change', (event) => {
+    let value = event.target.value;
+
+    $.get("http://localhost:8081/station?source="+value, (data) => {
+        //set departureStationData
+        arrivalStationData["lat"] = data["root"]["stations"]["station"]["gtfs_latitude"];
+        arrivalStationData["long"] = data["root"]["stations"]["station"]["gtfs_longitude"];
+    });
+});
+
+function initMap() {
+    window.directionsService = new google.maps.DirectionsService();
+    window.directionsDisplay = new google.maps.DirectionsRenderer();
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.773972, lng: -122.431297},
+        zoom: 10
+    });
+    directionsDisplay.setMap(map);
+}
+
+function setDirections() {
+    //create start and end LatLng
+    let start = new google.maps.LatLng(departureStationData["lat"], departureStationData["long"]);
+    let end = new google.maps.LatLng(arrivalStationData["lat"], arrivalStationData["long"]);
+
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: 'TRANSIT',
+        transitOptions: {
+            modes:['TRAIN']
+        }
+    }
+
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+    });
+}
 //add on change listener
-$("#startStation").on('change', (event) => {
+startStation.on('change', (event) => {
     // define value
     let value = event.target.value;
 
@@ -37,7 +119,11 @@ $("#startStation").on('change', (event) => {
     if(value !== "Select") {
         let htmlString = "<h2>Departure Station Info: </h2>";
         //make a call to the API to get station details
-        $.get("http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig="+value+"&key=MW9S-E7SL-26DU-VV8V&json=y", (data) => {
+        $.get("http://localhost:8081/station?source="+value, (data) => {
+            // //set departureStationData
+            departureStationData["lat"] = data["root"]["stations"]["station"]["gtfs_latitude"];
+            departureStationData["long"] = data["root"]["stations"]["station"]["gtfs_longitude"];
+
             let stationData = data["root"]["stations"]["station"];
             //loop through the stationInfoMap
             for(let key in stationInfoMap) {
@@ -49,7 +135,7 @@ $("#startStation").on('change', (event) => {
                 //get current val
                 let val = "";
                 if(key === "intro") {
-                    val = stationData[key]["#cdata-section"];
+                    val = stationData[key]["__cdata"];
                 } else {
                     val = stationData[key];
                 }
@@ -69,16 +155,87 @@ $("#startStation").on('change', (event) => {
 
 //add event listener to the submit button
 submit.on('click', () => {
+    setSchedule();
+});
+
+setSchedule = () => {
     //get the departure and arrival station
     let departureStation = startStation.val();
     let arrivalStation = endStation.val();
 
     //make the call to get the valid schedule
-    $.get('http://api.bart.gov/api/sched.aspx?cmd=depart&orig='+departureStation+'&dest='+arrivalStation+'&key=MW9S-E7SL-26DU-VV8V&json=y', (data) => {
-        console.log(data);
+    let rows = "";
+    table.html('<tr><th>Departure Time</th><th>Fare</th><th>Time of Arrival</th></tr>');
+    $.get('http://localhost:8081/trips?source='+departureStation+'&dest='+arrivalStation, (data) => {
         let scheduleData = data["root"]["schedule"]["request"]["trip"];
         $.each(scheduleData, (index)=> {
-            console.log(scheduleData[index]);
+            let row = "";
+            if(index === 0) {
+                row = "<tr class='row first'>";
+            } else {
+                row = "<tr class='row'>";
+            }
+        
+            //set departure time
+            row+="<td>"+scheduleData[index]["origTimeMin"]+"</td>";
+
+            //set fare
+            row+="<td>$"+scheduleData[index]["fares"]["fare"]["amount"]+"</td>";
+
+            //set arrival time
+            if(index === 0) {
+                //set the fistArrivalTime
+                firstAvailableTrain = scheduleData[index]["origTimeMin"];
+            }
+            row+="<td>"+scheduleData[index]["destTimeMin"]+"</td>";
+
+            //close row
+            row+="</tr>";
+            rows+=row;
         });
+
+        //set the inner div html
+        timeRemainingDiv.show();
+        tableDiv.show();
+        setDepartureTrainTimeInSeconds();
+        startTimer();
+        setDirections();
+        table.append(rows);
     })
-});
+}
+
+setDepartureTrainTimeInSeconds = () => {
+    //now
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes();
+    let splitNow = now.split(":");
+    if(parseInt(splitNow[0], 10) > 12) {
+        splitNow[0] = (parseInt(splitNow[0], 10) - 12).toString();
+    }
+    now = splitNow.join(':');
+    
+    //seconds diff
+    let nowSeconds = convertTimeToSeconds(now);
+    let firstAvailableTrainSeconds = convertTimeToSeconds(firstAvailableTrain);
+    let diff = firstAvailableTrainSeconds - nowSeconds;
+
+    timeRemaining.text(diff);
+}
+
+startTimer = () => {
+    let seconds = parseInt(timeRemaining.text(), 10);
+    trainTimer(seconds);
+}
+
+convertTimeToSeconds = (timeString) => {
+    if(timeString.includes("AM") || timeString.includes("PM")) {
+        timeString = timeString.replace("AM", "");
+        timeString = timeString.replace("PM", "");
+    }
+    //split timeString
+    let split = timeString.split(":");
+
+    //calculate in seconds
+    let seconds = split[0] * 60 * 60 + split[1] * 60;
+    return seconds;
+}
--- server.js
+++ server.js
@@ -3,13 +3,53 @@ var http = require('http');
 var app = express();
 app.use(express.static("public"));
 var server = http.createServer(app);
+var request = require('request');
+var parser = require('fast-xml-parser');
 var port = 8081;
 
+var options = {
+    attributeNamePrefix : "",
+    // attrNodeName: "attr", //default is 'false'
+    textNodeName : "#text",
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : false,
+    parseNodeValue : true,
+    parseAttributeValue : true,
+    trimValues: true,
+    cdataTagName: "__cdata", //default is 'false'
+    cdataPositionChar: "\\c",
+    localeRange: "", //To support non english character in tag/attribute values.
+    parseTrueNumberOnly: false,
+};
 
-app.get('/', function(req,res) {
-    res.send("Hello World");
+
+//GET stations
+app.get('/stations', function(req,res) {
+    request('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V', (err, result) => {
+        let jsonObj = parser.parse(result.body,options);
+        res.send(jsonObj);
+    });
 });
 
+//GET station info
+app.get('/station', (req, res) => {
+    let stationAbbr = req.query.source;
+    request('http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig='+stationAbbr+'&key=MW9S-E7SL-26DU-VV8V', (err, result) => {
+        let jsonObj = parser.parse(result.body,options);
+        res.send(jsonObj);
+    })
+});
+
+app.get('/trips', (req,res) => {
+    let source = req.query.source;
+    let dest = req.query.dest;
+
+    request('http://api.bart.gov/api/sched.aspx?cmd=depart&orig='+source+'&dest='+dest+'&a=4&b=0&key=MW9S-E7SL-26DU-VV8V', (err, result) => {
+        let jsonObj = parser.parse(result.body,options);
+        res.send(jsonObj);
+    });
+});
 
 //listen
 server.listen(port, function(){
